8a5a807c2277dcc4991d0fbf59e049cb5a56d8bf
Wasya1212 <wasya1212cool@gmail.com>
2024-02-11 15:07:30 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://wasya1212.github.io/valentine/'
+  plugins: [react()]
 })
